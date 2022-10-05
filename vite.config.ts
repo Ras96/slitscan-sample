daeb279eba7eb9977da7a455e0be71fbbde2c8bb
Ras96 <asymptote.k.k@gmail.com>
2022-10-05 21:23:28 +0900

    :wrench: include .eslintrc.yml and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +9,5 @@ export default defineConfig({
   build: {
     outDir: './dist',
   },
-  plugins: [react()]
+  plugins: [react()],
 })
