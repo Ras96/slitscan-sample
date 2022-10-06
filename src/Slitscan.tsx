552452961a4b59ec2ac2ce1aad9de373c6477493
Ras96 <asymptote.k.k@gmail.com>
2022-10-07 00:36:55 +0900

    :sparkles: impl video slit scan

--- src/Slitscan.tsx
+++ src/Slitscan.tsx
@@ -1,56 +1,103 @@
-import { useEffect, useState } from 'react'
-// images from http://placeimg.com/
-import animal0 from './assets/animal0.png'
-import animal1 from './assets/animal1.png'
-import animal2 from './assets/animal2.png'
-import animal3 from './assets/animal3.png'
-import animal4 from './assets/animal4.png'
-import animal5 from './assets/animal5.png'
-import animal6 from './assets/animal6.png'
-import animal7 from './assets/animal7.png'
-import animal8 from './assets/animal8.png'
-import animal9 from './assets/animal9.png'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import sampleVideo from './assets/sample-from-pexels.mp4'
 
-const width = 1000
-const height = 500
-const animals = [animal0, animal1, animal2, animal3, animal4, animal5, animal6, animal7, animal8, animal9]
-
-function Slitscan() {
-  const [context, setContext] = useState<CanvasRenderingContext2D | null>(null)
+const useAnimationFrame = (callback: () => void) => {
+  const reqIdRef = useRef<number>(0)
+  const loop = useCallback(() => {
+    // NOTE:
+    // requestAnimationFrame requests only one frame at a time,
+    // so we need to call it recursively.
+    reqIdRef.current = requestAnimationFrame(loop)
+    callback()
+  }, [callback])
 
   useEffect(() => {
-    const canvas = document.getElementById('canvas') as HTMLCanvasElement
-    const canvasContext = canvas.getContext('2d') as CanvasRenderingContext2D
+    reqIdRef.current = requestAnimationFrame(loop)
+    return () => cancelAnimationFrame(reqIdRef.current)
+  }, [loop])
+}
+
+function Slitscan() {
+  const [context, setContext] = useState<CanvasRenderingContext2D>()
+  const [bufferContext, setBufferContext] = useState<CanvasRenderingContext2D>()
+  const [video, setVideo] = useState<HTMLVideoElement>()
+  const [frames, setFrames] = useState<ImageData[]>([])
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [maxSlices] = useState(200)
+
+  useEffect(() => {
+    const canvas = document.getElementById('canvas-main') as HTMLCanvasElement
+    const canvasContext = canvas.getContext('2d', { willReadFrequently: true }) as CanvasRenderingContext2D
     setContext(canvasContext)
   }, [])
 
   useEffect(() => {
-    if (context) {
-      for (let i = 0; i < animals.length; i++) {
-        const img = new Image()
-        img.src = animals[i]
-        img.onload = () => {
-          context.drawImage(
-            img,
-            // source x, y, width, height
-            100 * i,
-            0,
-            100,
-            500,
-            // destination x, y, width, height
-            100 * i,
-            0,
-            100,
-            500
-          )
-        }
-      }
+    const canvas = document.getElementById('canvas-buffer') as HTMLCanvasElement
+    const canvasContext = canvas.getContext('2d', { willReadFrequently: true }) as CanvasRenderingContext2D
+    setBufferContext(canvasContext)
+  }, [])
+
+  useEffect(() => {
+    const videoElement = document.getElementById('video') as HTMLVideoElement
+    videoElement.src = sampleVideo
+    videoElement.onloadeddata = () => {
+      setVideo(videoElement)
+      setWidth(videoElement.videoWidth)
+      setHeight(videoElement.videoHeight)
     }
-  }, [context])
+  }, [])
+
+  // This is the main function that does the slitscan effect.
+  useAnimationFrame(
+    useCallback(() => {
+      if (!video || !context || !bufferContext) {
+        return
+      }
+
+      // draw the current video frame to the buffer canvas
+      bufferContext.drawImage(video, 0, 0, width, height, 0, 0, width, height)
+
+      // add the current frame to the `frames` and remove the old one
+      const currentFrames = frames
+      currentFrames.push(bufferContext.getImageData(0, 0, width, height))
+      while (currentFrames.length > maxSlices) {
+        currentFrames.shift()
+      }
+      setFrames(currentFrames)
+
+      // draw the parts of the frames to the main canvas
+      for (let i = 0; i < maxSlices; i++) {
+        context.putImageData(
+          frames[i],
+          // x, y 
+          0,
+          0,
+          // x, y, width, height of the rectangle to be drawn
+          (width * i) / maxSlices,
+          0,
+          Math.ceil(width / maxSlices),
+          height
+        )
+      }
+    }, [context, bufferContext, video, frames, width, height, maxSlices])
+  )
 
   return (
     <div>
-      <canvas id="canvas" width={width} height={height}></canvas>
+      <div>
+        <h1>Original</h1>
+        <video id="video" width={width} height={height} autoPlay loop muted></video>
+      </div>
+      <div>
+        <h1>Slitscan-ed</h1>
+        <canvas id="canvas-main" width={width} height={height}></canvas>
+      </div>
+
+      {/* hidden elements */}
+      <div>
+        <canvas id="canvas-buffer" width={width} height={height} hidden></canvas>
+      </div>
     </div>
   )
 }
